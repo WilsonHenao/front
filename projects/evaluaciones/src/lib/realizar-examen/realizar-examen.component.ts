6483208e8ff4427326179968bf2be7871094cf7a
Wilson <wilson.henao@utp.edu.co>
2020-06-26 00:02:47 -0500

    Ajustes

--- projects/evaluaciones/src/lib/realizar-examen/realizar-examen.component.ts
+++ projects/evaluaciones/src/lib/realizar-examen/realizar-examen.component.ts
@@ -1,6 +1,8 @@
+import { FormGroup, FormBuilder, Validators, FormArray } from '@angular/forms';
 import { Component, OnInit, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA, MatDialogConfig } from '@angular/material/dialog';
 import { ServiceService } from '../services/service.service';
+import {MatSnackBar} from '@angular/material/snack-bar';
 
 export class Questions {
   question: any;
@@ -16,8 +18,9 @@ export class RealizarExamenComponent implements OnInit {
   options: any;
   question: Array<Questions> = [];
   ngModelOption: any;
+  examGroup: FormGroup;
   constructor(private dialogRef: MatDialogRef<RealizarExamenComponent>, @Inject(MAT_DIALOG_DATA) public data: any,
-              private service: ServiceService) {
+              private service: ServiceService, private formBuilder: FormBuilder, private alerta: MatSnackBar) {
     dialogRef.disableClose = true;
    }
 
@@ -25,6 +28,45 @@ export class RealizarExamenComponent implements OnInit {
 
   ngOnInit(): void {
     this.getQuestions(this.data.exam.id);
+    this.examGroup = this.formBuilder.group({
+      answers: this.formBuilder.array([])
+    });
+  }
+
+  createAnswer(): FormGroup {
+    return this.formBuilder.group({
+      answerCtrl: [''],
+      questionCtrl: ['']
+    });
+  }
+
+  save(value){
+    console.log(value);
+  }
+
+  saveAnswer(){
+    this.validateAnswers();
+    console.log(this.examGroup.value);
+  }
+
+  validateAnswers(){
+    // tslint:disable-next-line: prefer-for-of
+    let valoracion = 0;
+    for (let index = 0; index < this.examGroup.value.answers.length; index++) {
+      if (this.examGroup.value.answers[index].answerCtrl.question === this.questions[index].id) {
+        if (this.examGroup.value.answers[index].answerCtrl.options === this.questions[index].correctAnswer) {
+          valoracion = ((this.data.exam.maximumNote) * (this.questions[index].assessment * 0.01)) + valoracion;
+          console.log(valoracion);
+        }
+      }
+    }
+    this.openAlerta(valoracion);
+  }
+
+  openAlerta(valoracion) {
+    this.alerta.open('Su nota es ' +  valoracion, null, {
+      duration: 4000,
+    });
   }
 
   getQuestions(i) {
@@ -32,6 +74,7 @@ export class RealizarExamenComponent implements OnInit {
       this.questions = success;
       this.questions.forEach(element => {
         this.getOptions(element);
+        this.allAnswers.push(this.createAnswer());
       });
     }, error => {
       console.log(error);
@@ -44,9 +87,12 @@ export class RealizarExamenComponent implements OnInit {
     this.service.getOption(element.id).subscribe(success => {
       q.option = success;
       this.question.push(q);
-      console.log(this.question);
     }, error => {
       console.log(error);
     });
+  }
+
+  get allAnswers(): FormArray {
+    return this.examGroup.get('answers') as FormArray;
   }
 }
